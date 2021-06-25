1d2cadb3df468c4953c8c45f4ca41e35488bb31f
Gesta Putra Gemilang <gestaputrag@gmail.com>
2021-06-25 18:13:44 +0700

    menambahkan halaman login

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const { title } = require("process");
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(mainRoutes);
 
--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const path = require("path");
 const router = express.Router();
 
 router.get("/login", (req, res, next) => {
-  res.send("Hi you are at LOGIN PAGE");
+  res.sendFile("login.html", { root: path.join(__dirname, "../", "views") });
+});
+
+router.post("/login", (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  console.log(email, password);
+  res.redirect("/game");
 });
 
 module.exports = router;
