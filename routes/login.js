cb989e29c9f6031e8fc4cd467a73f80e94cedeeb
Gesta Putra Gemilang <gestaputrag@gmail.com>
2021-06-25 18:34:13 +0700

    change login.html file to view engine ejs

--- routes/login.js
+++ routes/login.js
@@ -3,13 +3,14 @@ const path = require("path");
 const router = express.Router();
 
 router.get("/login", (req, res, next) => {
-  res.sendFile("login.html", { root: path.join(__dirname, "../", "views") });
+  const title = "Login";
+  const style = "login.css";
+  res.render("layouts/login", { title, style });
 });
 
 router.post("/login", (req, res, next) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  console.log(email, password);
+  const data = req.body;
+  console.log(data);
   res.redirect("/game");
 });
 
