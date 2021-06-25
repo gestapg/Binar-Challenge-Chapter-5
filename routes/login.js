d08f0d372e3baeff6c56ef14f932eeff19f5c567
Gesta Putra Gemilang <gestaputrag@gmail.com>
2021-06-25 23:05:29 +0700

    save login data to json file

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,11 @@
 const express = require("express");
-const path = require("path");
 const router = express.Router();
+
+const fs = require("fs");
+const path = require("path");
+
+const pathFile = path.join(path.dirname(process.mainModule.filename), "data", "user.json");
+const pathDir = path.join(path.dirname(process.mainModule.filename), "data");
 
 router.get("/login", (req, res, next) => {
   const title = "Login";
@@ -10,7 +15,16 @@ router.get("/login", (req, res, next) => {
 
 router.post("/login", (req, res, next) => {
   const data = req.body;
-  console.log(data);
+  fs.readFile(pathFile, (err, user) => {
+    let dataUser = [];
+    if (err) {
+      fs.mkdir(pathDir, (err) => {});
+    } else {
+      dataUser = JSON.parse(user);
+    }
+    dataUser.push(data);
+    fs.writeFile(pathFile, JSON.stringify(dataUser), (err) => {});
+  });
   res.redirect("/game");
 });
 
